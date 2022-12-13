dda7f609ec18d8d5b41c669236e0c80185b201d8
강현석 <hs@ganghyeonseog-ui-MacBookAir.local>
2022-12-14 00:23:28 +0900

    endShare - 테스트로 생성된 ShareCode 중복 해결

--- relief-domain/src/main/java/com/sg/relief/domain/persistence/repository/ShareCodeRepository.java
+++ relief-domain/src/main/java/com/sg/relief/domain/persistence/repository/ShareCodeRepository.java
@@ -4,10 +4,12 @@ import com.sg.relief.domain.persistence.entity.ShareCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ShareCodeRepository extends JpaRepository<ShareCode, Long> {
     Optional<ShareCode> findByCode(String code);
     Optional<ShareCode> findByUserId(String userId);
+    List<ShareCode> findAllByUserId(String userId);
 }
