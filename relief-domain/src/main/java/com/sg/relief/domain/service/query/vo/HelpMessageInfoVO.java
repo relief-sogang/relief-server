5ab3e79c0e20a77681411499aecbd8e0dc0af614
강현석 <hs@ganghyeonseog-ui-MacBookAir.local>
2022-12-13 23:02:45 +0900

    helpmessage setting 확인 기능

---
+++ relief-domain/src/main/java/com/sg/relief/domain/service/query/vo/HelpMessageInfoVO.java
@@ -0,0 +1,12 @@
+package com.sg.relief.domain.service.query.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class HelpMessageInfoVO {
+    private String message;
+}
